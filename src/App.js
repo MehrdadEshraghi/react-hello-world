32f72e4e3b49d738d04e12e66ea7bfb9fafd7d13
Mehrdad Eshraghi <67071121+MehrdadEshraghi@users.noreply.github.com>
2021-12-13 01:59:57 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React forever yoooooooooooooo
         </a>
       </header>
     </div>
