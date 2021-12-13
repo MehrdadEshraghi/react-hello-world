a4c4be9fc6ea251c6f9f7565a2da08be91ca2228
Mehrdad Eshraghi <67071121+MehrdadEshraghi@users.noreply.github.com>
2021-12-13 03:30:48 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React forever yoooooooooooooo
+          Fu**k jenkins
         </a>
       </header>
     </div>
