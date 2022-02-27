bc8591ea590ce27559b2f4524ad5886dc75fb79c
Mehrdad Eshraghi <67071121+MehrdadEshraghi@users.noreply.github.com>
2022-02-27 11:53:05 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I le jenhffhhkins.
+          I le jenhffhphkins.
         </a>
       </header>
     </div>
