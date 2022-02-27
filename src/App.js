15d16f45229eaff2335d34ff702ddbc461f71c1d
Mehrdad Eshraghi <67071121+MehrdadEshraghi@users.noreply.github.com>
2022-02-27 12:15:37 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I love jenkins
+          I love jenkins..
         </a>
       </header>
     </div>
